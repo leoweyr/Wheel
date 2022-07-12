83cdedf60e4410a99a223d1aa94b12e104a1635b
leoweyr <2516427163@qq.com>
2022-07-12 23:50:13 +0800

    Rebuild JsonCompare:function<Json::compare> into class<Json::Comparer> and new method<isSame>

--- Source/Jsoncpp/JsonCompare.cpp
+++ Source/Jsoncpp/JsonCompare.cpp
@@ -1,18 +1,16 @@
 #include "JsonCompare.h"
 
-Json::Value Json::Compare(const Json::Value compared, const Json::Value referred, Json::Value &same) {
+Json::Value Json::Comparer::compare(const Json::Value compared, const Json::Value referred, Json::Value &same) {
+    Json::Value::Members compared_keys = compared.getMemberNames(), referred_keys = referred.getMemberNames();
     Json::Value differences, subSame, subDifferences;
-    int differencesCount = 0;
-    Json::Value::Members compared_keys = compared.getMemberNames();
-    Json::Value::Members referred_keys = referred.getMemberNames();
-    int compared_keys_iter_traversed;
+    int differencesCount = 0, compared_keys_iter_traversed;
     for(Json::Value::Members::iterator referred_keys_iter = referred_keys.begin(); referred_keys_iter != referred_keys.end(); referred_keys_iter++){
         compared_keys_iter_traversed = 0;
         for(Json::Value::Members::iterator compared_keys_iter = compared_keys.begin(); compared_keys_iter != compared_keys.end(); compared_keys_iter++){
             if(*compared_keys_iter == *referred_keys_iter){
                 if(compared[*compared_keys_iter].isObject() == 1 && referred[*referred_keys_iter].isObject() == 1){
                     if(compared[*compared_keys_iter].getMemberNames().size() != 0 && referred[*referred_keys_iter].getMemberNames().size() != 0) {
-                        subDifferences = Json::Compare(compared[*compared_keys_iter], referred[*referred_keys_iter], subSame);
+                        subDifferences = compare(compared[*compared_keys_iter], referred[*referred_keys_iter], subSame);
                         differences[*referred_keys_iter] = subDifferences;
                         same[*referred_keys_iter] = subSame;
                         break;
@@ -29,5 +27,26 @@ Json::Value Json::Compare(const Json::Value compared, const Json::Value referred
         }
     }
     differences["DIFFERENCESCOUNT"] = differencesCount;
+    m_differences = differences;
+    m_differences_roll = m_differences;
     return differences;
+}
+
+bool Json::Comparer::isSame(const std::string key = "") {
+    Json::Value differences = (key.size() == 0)?(m_differences_roll):(m_differences_roll[key]), differences_roll_backup;
+    Json::Value::Members differences_keys = differences.getMemberNames();
+    if(m_differences_roll["DIFFERENCESCOUNT"] != 0){
+        return false;
+    }
+    for(Json::Value::Members::iterator iter = differences_keys.begin();iter != differences_keys.end();iter++){
+        if(m_differences_roll[*iter].isObject() == 1){
+            differences_roll_backup = m_differences_roll;
+            m_differences_roll = m_differences_roll[*iter];
+            if(isSame(*iter) == false){
+                return false;
+            }
+            m_differences_roll = differences_roll_backup;
+        }
+    }
+    return true;
 }
--- Source/Jsoncpp/JsonCompare.h
+++ Source/Jsoncpp/JsonCompare.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <string>
+
 #include "Jsoncpp.h"
 
 namespace Json{
-    Json::Value Compare(const Json::Value compared, const Json::Value referred, Json::Value &same);
+    class Comparer{
+        private:
+            Json::Value m_differences;
+            Json::Value m_differences_roll;
+        public:
+            //The method name design caters to the original habit of Jsoncpp ontology.
+            Json::Value compare(const Json::Value compared, const Json::Value referred, Json::Value &same);
+            bool isSame(const std::string key);
+    };
 }
