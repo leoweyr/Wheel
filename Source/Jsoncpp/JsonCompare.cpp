306671dbc1eb15704ccb448a490323a7e84c395f
leoweyr <2516427163@qq.com>
2022-07-15 11:15:45 +0800

    Rebuild JsonCompare:method<Json::compare> from class<Comparer>, convert param<same> and (return:Json::Value) to class<Comparer> public members.

--- Source/Jsoncpp/JsonCompare.cpp
+++ Source/Jsoncpp/JsonCompare.cpp
@@ -1,8 +1,10 @@
 #include "JsonCompare.h"
 
-Json::Value Json::Comparer::compare(const Json::Value compared, const Json::Value referred, Json::Value &same) {
+void Json::Comparer::compare(const Json::Value compared, const Json::Value referred) {
+    m_jsonLevel ++;
     Json::Value::Members compared_keys = compared.getMemberNames(), referred_keys = referred.getMemberNames();
-    Json::Value differences, subSame, subDifferences;
+    Json::Value differencesLevel, sameLevel;
+    bool isRollObject = false;
     int differencesCount = 0, compared_keys_iter_traversed;
     for(Json::Value::Members::iterator referred_keys_iter = referred_keys.begin(); referred_keys_iter != referred_keys.end(); referred_keys_iter++){
         compared_keys_iter_traversed = 0;
@@ -10,13 +12,15 @@ Json::Value Json::Comparer::compare(const Json::Value compared, const Json::Valu
             if(*compared_keys_iter == *referred_keys_iter){
                 if(compared[*compared_keys_iter].isObject() == 1 && referred[*referred_keys_iter].isObject() == 1){
                     if(compared[*compared_keys_iter].getMemberNames().size() != 0 && referred[*referred_keys_iter].getMemberNames().size() != 0) {
-                        subDifferences = compare(compared[*compared_keys_iter], referred[*referred_keys_iter], subSame);
-                        differences[*referred_keys_iter] = subDifferences;
-                        same[*referred_keys_iter] = subSame;
+                        compare(compared[*compared_keys_iter], referred[*referred_keys_iter]);
+                        m_jsonLevel --;
+                        differencesLevel[*referred_keys_iter] = m_differences_roll_compare;
+                        sameLevel[*referred_keys_iter] = m_same_roll_compare;
+                        isRollObject = true;
                         break;
                     }
                 }else if(compared[*compared_keys_iter] == referred[*referred_keys_iter]){
-                    same[*referred_keys_iter] = referred[*referred_keys_iter];
+                    m_same_roll_compare[*referred_keys_iter] = referred[*referred_keys_iter];
                     break;
                 }
             }
@@ -26,26 +30,33 @@ Json::Value Json::Comparer::compare(const Json::Value compared, const Json::Valu
             }
         }
     }
-    differences["DIFFERENCESCOUNT"] = differencesCount;
-    m_differences = differences;
-    m_differences_roll = m_differences;
-    return differences;
+    differencesLevel["DIFFERENCESCOUNT"] = differencesCount;
+    if(isRollObject == true) {
+        m_same_roll_compare = sameLevel;
+    }
+    m_differences_roll_compare = differencesLevel;
+    if(m_jsonLevel == 1){
+        differences = m_differences_roll_compare;
+        same = m_same_roll_compare;
+        m_jsonLevel = 0;
+        m_differences_roll_isSame = differences;
+    }
 }
 
 bool Json::Comparer::isSame(const std::string key = "") {
-    Json::Value differences = (key.size() == 0)?(m_differences_roll):(m_differences_roll[key]), differences_roll_backup;
+    Json::Value differences = (key.size() == 0) ? (m_differences_roll_isSame) : (m_differences_roll_isSame[key]), differencesLevel;
     Json::Value::Members differences_keys = differences.getMemberNames();
-    if(m_differences_roll["DIFFERENCESCOUNT"] != 0){
+    if(m_differences_roll_isSame["DIFFERENCESCOUNT"] != 0){
         return false;
     }
     for(Json::Value::Members::iterator iter = differences_keys.begin();iter != differences_keys.end();iter++){
-        if(m_differences_roll[*iter].isObject() == 1){
-            differences_roll_backup = m_differences_roll;
-            m_differences_roll = m_differences_roll[*iter];
+        if(m_differences_roll_isSame[*iter].isObject() == 1){
+            differencesLevel = m_differences_roll_isSame;
+            m_differences_roll_isSame = m_differences_roll_isSame[*iter];
             if(isSame(*iter) == false){
                 return false;
             }
-            m_differences_roll = differences_roll_backup;
+            m_differences_roll_isSame = differencesLevel;
         }
     }
     return true;
--- Source/Jsoncpp/JsonCompare.h
+++ Source/Jsoncpp/JsonCompare.h
@@ -7,11 +7,18 @@
 namespace Json{
     class Comparer{
         private:
-            Json::Value m_differences;
-            Json::Value m_differences_roll;
+            //Prevent conflict between asynchronous simultaneous outputs (member:public)<differences> and (member:public)<same> and handler method tasks.
+            int m_jsonLevel = 0;
+            Json::Value m_differences_roll_compare;
+            Json::Value m_differences_roll_isSame;
+            Json::Value m_same_roll_compare;
+
         public:
-            //The method name design caters to the original habit of Jsoncpp ontology.
-            Json::Value compare(const Json::Value compared, const Json::Value referred, Json::Value &same);
+            //The public member & method name design caters to the original habit of Jsoncpp ontology.
+            Json::Value differences;
+            Json::Value same;
+
+            void compare(const Json::Value compared, const Json::Value referred);
             bool isSame(const std::string key);
     };
 }
