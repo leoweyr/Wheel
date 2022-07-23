a40ff6cde0848d159bc085d3db4fbf43274ea440
leoweyr <2516427163@qq.com>
2022-07-23 18:23:12 +0800

    Update Bitscode:New function<StringUnite>,rebuild function<StringSplit>.

--- Lib/Bitscode/Bitscode.h
+++ Lib/Bitscode/Bitscode.h
@@ -8,4 +8,6 @@ void CharToString(std::string &dest_str,char *source_charArray);
 
 void StringToChar(char *dest_charArray,const std::string &source_str);
 
-std::vector<std::string> StringSplit(const std::string &str,const std::string &pattern);
+std::vector<std::string> StringSplit(const std::string unitedString, const std::string pattern);
+
+std::string StringUnite(const std::vector<std::string> splitStringArray, const std::string pattern);
--- Source/Bitscode/Bitscode.cpp
+++ Source/Bitscode/Bitscode.cpp
@@ -18,22 +18,23 @@ void StringToChar(char *dest_charArray,const std::string &source_str){
     *dest_charArray = '\0';
 }
 
-std::vector<std::string> StringSplit(const std::string &str,const std::string &pattern)
-{
-    std::vector<std::string> strUnitGroup;
-    if ("" == str)
-    {
-        return strUnitGroup;
+std::vector<std::string> StringSplit(const std::string unitedString, const std::string pattern){
+    std::vector<std::string> splitStringArray;
+    if (unitedString != ""){
+        const_cast<std::string &>(unitedString) = unitedString + pattern;
+        for (size_t pattern_pos = unitedString.find(pattern);pattern_pos != std::string::npos;pattern_pos = unitedString.find(pattern)){
+            std::string splitStringUnit = unitedString.substr(0, pattern_pos);
+            splitStringArray.push_back(splitStringUnit);
+            const_cast<std::string &>(unitedString) = unitedString.substr(pattern_pos + pattern.size(), unitedString.size());
+        }
     }
-    std::string strWithPattern = str + pattern;
-    size_t pos = strWithPattern.find(pattern);
-    size_t size = strWithPattern.size();
-    while (pos != std::string::npos)
-    {
-        std::string strUnit = strWithPattern.substr(0,pos);
-        strUnitGroup.push_back(strUnit);
-        strWithPattern = strWithPattern.substr(pos + pattern.size(), size);
-        pos = strWithPattern.find(pattern);
+    return splitStringArray;
+}
+
+std::string StringUnite(const std::vector<std::string> splitStringArray, const std::string pattern){
+    std::string UnitedString = "";
+    for(std::vector<std::string>::iterator splitStringArray_iter = const_cast<std::vector<std::string> &>(splitStringArray).begin();splitStringArray_iter != splitStringArray.end();splitStringArray_iter++){
+        UnitedString += (*splitStringArray_iter) + pattern;
     }
-    return strUnitGroup;
+    return UnitedString.substr(0,UnitedString.size() - pattern.size());
 }
